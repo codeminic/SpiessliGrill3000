74e4703780dd8ee575e25424d67e257c5a8ef275
Dominic Sauder <dominic.sauder@solve.ch>
2019-05-03 18:40:57 +0200

    add text-to-speech

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function addPerson() {
     let name = $('#nameInput').val();
     $('#nameInput').val('');
 
+    say('Hallo ' + name + '! Willkommen beim Spiessli Master 3000!');
+
     $('#table tr:last').after('<tr><td>' + name + '</td>' +
         '<td>' +
-        '<button onclick="addTimer(\'' + name + '\', 5)">5 min</button>' +
+        '<button onclick="addTimer(\'' + name + '\', 1)">5 min</button>' +
         '<button onclick="addTimer(\'' + name + '\', 10)">10 min</button>' +
         '<button onclick="addTimer(\'' + name + '\', 15)">15 min</button>' +
         '</td></tr>'
@@ -20,6 +22,13 @@ function addPerson() {
 function addTimer(person, duration) {
     console.log('Starting timer for ' + person);
     setTimeout(() => {
-        console.log('Timer completed for ' + person)
+        console.log('Timer for ' + person + ' exceeded!');
+        say('Hey ' + person + '. Dein Spiessli ist vielleicht fertig!');
     }, duration * 1000 * 60);
+}
+
+function say(text) {
+    var speakSynthesis = new SpeechSynthesisUtterance(text);
+    speakSynthesis.lang = "de-DE";
+    window.speechSynthesis.speak(speakSynthesis);
 }
