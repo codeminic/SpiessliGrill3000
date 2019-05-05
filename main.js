a5562e9f6f48f8873bf7481387340b0dc56f351e
Dominic Sauder <dominic.sauder@gmail.com>
2019-05-05 18:17:58 +0200

    dont use name to build html tags

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var persons = new Object();
+
 $('document').ready(() => {
     $('#addPersonButton').on('click', function () {
         addPerson();
@@ -6,45 +8,46 @@ $('document').ready(() => {
 
 function addPerson() {
     let person = $('#nameInput').val();
+    let id = uuidv4();
     $('#nameInput').val('');
-    
+    persons[id] = person;
+
     say(getRandomGreeting() + ', ' + person + '. Willkommen beim Spiessli Master 3000! Schön das du da bist! Neben deinem Namen kannst du' +
     'einen Timer stellen, und ich erinnere dich sobald dieser abgelaufen ist');
     say('Geniess die Party!' + getRandomCheers());
 
-    $('#tableBody').append('<tr id="row' + person + '"><td>' + person + '</td>' +
-        '<td id="timer' + person + '">' +
+    $('#tableBody').append('<tr id="row' + id + '"><td>' + person + '</td>' +
+        '<td id="timer' + id + '">' +
         '</td>' + 
         '<td align="right">' +
-            '<img onclick="deletePerson(\'' + person + '\')" src="https://img.icons8.com/color/48/000000/close-window.png">' +
+            '<img onclick="deletePerson(\'' + id + '\')" src="https://img.icons8.com/color/48/000000/close-window.png">' +
         '</td>' + 
         '</tr>'
     );
-    $('#timer' + person).html(buildTimer(person));
+    $('#timer' + id).html(buildTimer(id));
 }
 
-function buildTimer(person){
-    var timer = '<button onclick="addTimer(\'' + person + '\', 5)" class="btn btn-primary">Test</button>' +
-                '<button onclick="addTimer(\'' + person + '\', 300)" class="btn btn-primary">5 min</button>' +
-                '<button onclick="addTimer(\'' + person + '\', 600)" class="btn btn-primary">10 min</button>' +
-                '<button onclick="addTimer(\'' + person + '\', 900)" class="btn btn-primary">15 min</button>';
+function buildTimer(personId){
+    var timer = '<button onclick="addTimer(\'' + personId + '\', 5)" class="btn btn-primary">Test</button>' +
+                '<button onclick="addTimer(\'' + personId + '\', 300)" class="btn btn-primary">5 min</button>' +
+                '<button onclick="addTimer(\'' + personId + '\', 600)" class="btn btn-primary">10 min</button>' +
+                '<button onclick="addTimer(\'' + personId + '\', 900)" class="btn btn-primary">15 min</button>';
     return timer;
 }
 
-function addTimer(person, duration) {
-    console.log('Starting timer for ' + person);
+function addTimer(personId, duration) {
+    console.log('Starting timer for ' + personId);
     var interval = setInterval(() => {
         duration--;
-        $('#timer' + person).text('Läuft ab in ' + displayTimer(duration));
-        console.log(duration);
+        $('#timer' + personId).text('Läuft ab in ' + displayTimer(duration));
     }, 1000);
     setTimeout(() => {
         clearInterval(interval);
-        console.log('Timer for ' + person + ' exceeded!');
-        say('Spiessli Alarm für ' + person + '. ' + getRandomReminder());
-        $('#timer' + person).text('Abgelaufen');
+        console.log('Timer for ' + personId + ' exceeded!');
+        say('Spiessli Alarm für ' + getNameById(personId) + '. ' + getRandomReminder());
+        $('#timer' + personId).text('Abgelaufen');
         setTimeout(() => {
-            $('#timer' + person).html(buildTimer(person));
+            $('#timer' + personId).html(buildTimer(personId));
         }, 5000);
     }, duration * 1000);
 }
@@ -56,13 +59,25 @@ function say(text) {
 }
 
 function getRandomGreeting(){
-    var greetings = ['Jo was geht ab', 'Sali', 'Hallo', 'Hej', 'Bonschorno'];
+    var greetings = [
+        'Jo was geht ab', 
+        'Sali', 
+        'Hallo', 
+        'Hej', 
+        'Bonschorno'
+    ];
 
     return getRandomElement(greetings);
 }
 
 function getRandomCheers(){
-    var cheers = ['Cheers', 'Zum Wohl', 'Salut', 'Viva', 'Uz uz uz uz', 'Ander di mango tri mi hani and mi'];
+    var cheers = [
+        'Cheers', 
+        'Zum Wohl', 
+        'Salut', 
+        'Viva', 
+        'Uz uz uz uz', 
+    ];
 
     return getRandomElement(cheers);
 }
@@ -93,4 +108,15 @@ function displayTimer(time){
 function deletePerson(person){
     console.log('Delete ' + person);
     $('#row' + person).remove();
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+function getNameById(id){
+    return persons[id];
 }
